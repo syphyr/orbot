7e8a4b509809920612b35260f7753bd13aa309ae
n8fr8 <nathan@freitas.net>
2017-12-07 15:45:23 -0500

    don't show app selection each time you turn VPN on/off

--- app/src/main/java/org/torproject/android/OrbotMainActivity.java
+++ app/src/main/java/org/torproject/android/OrbotMainActivity.java
@@ -352,13 +352,21 @@ public class OrbotMainActivity extends AppCompatActivity
                 }
             });
 
+			if (PermissionManager.isLollipopOrHigher()) {
 
-            findViewById(R.id.btnApps).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    enableVPN(true);
-                }
-            });
+                findViewById(R.id.btnApps).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        startActivityForResult(new Intent(OrbotMainActivity.this, AppManagerActivity.class), REQUEST_VPN_APPS_SELECT);
+
+                    }
+                });
+            }
+            else
+            {
+                findViewById(R.id.btnApps).setVisibility(View.GONE);
+            }
 		}
 		
 		
@@ -620,10 +628,7 @@ public class OrbotMainActivity extends AppCompatActivity
         Prefs.putUseVpn(enable);
 
         if (enable) {
-            if (PermissionManager.isLollipopOrHigher()) //let the user choose the apps
-                startActivityForResult(new Intent(OrbotMainActivity.this, AppManagerActivity.class), REQUEST_VPN_APPS_SELECT);
-            else
-                startActivity(new Intent(OrbotMainActivity.this, VPNEnableActivity.class));
+            startActivity(new Intent(OrbotMainActivity.this, VPNEnableActivity.class));
         } else
             stopVpnService();
     }
