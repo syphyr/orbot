7bd80f4527e4f0a2d101ac4b759852d5f8eca344
syphyr <syphyr@gmail.com>
2020-04-19 02:00:35 +0200

    Add build flag for standalone toolchain

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
+export ANDROID_TOOLCHAIN_HOME=~/src/android-ndk-r20b
+export ANDROID_NDK_HOME=~/toolchains/arm-linux-androideabi-4.9
+export PATH=$ANDROID_NDK_HOME/bin:$PATH
+
 cd orbotservice/src/main
 ndk-build NDK_PROJECT_PATH=.
 cd ../../..
-export ANDROID_NDK_HOME=~/src/android-ndk-r15c
 make -C external
 android update project --name Orbot --target android-23 --path .
 ./gradlew assemble
--- clean.sh
+++ clean.sh
@@ -1,6 +1,10 @@
+export ANDROID_TOOLCHAIN_HOME=~/src/android-ndk-r20b
+export ANDROID_NDK_HOME=~/toolchains/arm-linux-androideabi-4.9
+export PATH=$ANDROID_NDK_HOME/bin:$PATH
+
 cd orbotservice/src/main
 ndk-build NDK_PROJECT_PATH=. clean
 cd ../../..
-export ANDROID_NDK_HOME=~/src/android-ndk-r15c
 make -C external clean
 ./gradlew clean
+rm -rf build
