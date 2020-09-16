5968b94a757fa9bb1bd2bd1c1bb6acdef687c2fe
syphyr <syphyr@gmail.com>
2020-09-16 20:52:43 +0200

    Update tor to 0.4.4.5 and openssl bugfixes

--- orbotservice/src/main/java/org/torproject/android/service/TorServiceConstants.java
+++ orbotservice/src/main/java/org/torproject/android/service/TorServiceConstants.java
@@ -129,7 +129,7 @@ public interface TorServiceConstants {
     public static final String CMD_UPDATE_TRANS_PROXY = "update";
     public static final String CMD_SET_EXIT = "setexit";
 
-    public static final String BINARY_TOR_VERSION = "0.4.4.4-openssl1.1.1g";
+    public static final String BINARY_TOR_VERSION = "0.4.4.5-openssl1.1.1g";
     public static final String PREF_BINARY_TOR_VERSION_INSTALLED = "BINARY_TOR_VERSION_INSTALLED";
     
     //obfsproxy 
