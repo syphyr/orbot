09afc101468ef07c5894a74adce03e312b293826
syphyr <syphyr@gmail.com>
2020-09-18 02:31:05 +0200

    Update build scripts and kill java process

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
-export ANDROID_TOOLCHAIN_HOME=~/src/android-ndk-r20b
+export ANDROID_TOOLCHAIN_HOME=~/src/android-ndk-r21d
 export ANDROID_NDK_HOME=~/toolchains/arm-linux-androideabi-4.9
 export PATH=$ANDROID_NDK_HOME/bin:$PATH
 
@@ -7,4 +7,5 @@ ndk-build NDK_PROJECT_PATH=. clean
 cd ../../..
 make -C external clean
 ./gradlew clean
+./gradlew --stop
 rm -rf build
