4cb765e72e3924f63516402d7f13ff4dc3b46418
syphyr <syphyr@gmail.com>
2020-12-06 20:20:57 +0100

    Update build scripts

--- clean.sh
+++ clean.sh
@@ -1,6 +1,10 @@
+sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
+java -version
+javac -version
+
 export ANDROID_TOOLCHAIN_HOME=~/src/android-ndk-r21d
 export ANDROID_NDK_HOME=~/toolchains/arm-linux-androideabi-4.9
-export PATH=$ANDROID_NDK_HOME/bin:$PATH
+export PATH=$ANDROID_NDK_HOME/bin:$ANDROID_TOOLCHAIN_HOME:$PATH
 
 cd orbotservice/src/main
 ndk-build NDK_PROJECT_PATH=. clean
