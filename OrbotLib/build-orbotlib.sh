5bf7dff6f81d3f5c127ef1dfbce78ac8eaadeb67
syphyr <syphyr@gmail.com>
2023-02-08 23:45:41 +0100

    build-orbotlib: cleanup nits

--- OrbotLib/build-orbotlib.sh
+++ OrbotLib/build-orbotlib.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-rm -v *aar *jar
+rm -v *.aar *.jar
 if [ ! -d OrbotIPtProxy ]; then
    git clone https://github.com/guardianproject/OrbotIPtProxy.git
 fi
@@ -8,4 +8,3 @@ cd OrbotIPtProxy
 git fetch
 git rebase
 bash build-orbot.sh
-cd ..
