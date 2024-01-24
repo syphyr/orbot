4c625187f04c835b081d1790e524dba93276187b
syphyr <syphyr@gmail.com>
2024-01-24 03:06:19 +0100

    Adjust OrbotLib scripts
    This is done within IPtProxy now

--- OrbotLib/build-orbotlib.sh
+++ OrbotLib/build-orbotlib.sh
@@ -3,4 +3,3 @@
 rm *.aar
 cd OrbotIPtProxy
 bash build-orbot.sh
-mv OrbotLib.aar ..
--- OrbotLib/clean-orbotlib.sh
+++ OrbotLib/clean-orbotlib.sh
@@ -1,3 +1,19 @@
+if test -d "$TMPDIR"; then
+    :
+elif test -d "$TMP"; then
+    TMPDIR=$TMP
+elif test -d /var/tmp; then
+    TMPDIR=/var/tmp
+else
+    TMPDIR=/tmp
+fi
+
+TEMPDIR="$TMPDIR/IPtProxy"
+
+if [ -d "$TEMPDIR" ]; then
+    rm -rf "$TEMPDIR"
+fi
+
 cd OrbotIPtProxy/IPtProxy
 git clean -fdx
 cd snowflake
