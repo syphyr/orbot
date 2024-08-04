f4d72a867d39643ec7b9b029413a725b4c15aebb
Meenbeese <meenbeese@tutanota.com>
2024-08-04 10:27:06 -0400

    Refresh the navbar with new Material icons

--- app/src/main/java/org/torproject/android/OrbotActivity.kt
+++ app/src/main/java/org/torproject/android/OrbotActivity.kt
@@ -15,6 +15,7 @@ import android.widget.*
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 
@@ -30,11 +31,10 @@ import org.torproject.android.ui.LogBottomSheet
 
 class OrbotActivity : BaseActivity() {
 
-    private lateinit var bottomNavigationView: BottomNavigationView
-
     private lateinit var logBottomSheet: LogBottomSheet
-    lateinit var fragConnect: ConnectFragment
-    lateinit var fragMore: MoreFragment
+    var fragConnect: ConnectFragment = ConnectFragment()
+    var fragKindness: KindnessFragment = KindnessFragment()
+    var fragMore: MoreFragment = MoreFragment()
 
     var previousReceivedTorStatus: String? = null
 
@@ -79,11 +79,12 @@ class OrbotActivity : BaseActivity() {
 
         logBottomSheet = LogBottomSheet()
 
-        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        val navController: NavController = findNavController(R.id.nav_fragment)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
 
-        val navController = findNavController(R.id.nav_fragment)
         bottomNavigationView.setupWithNavController(navController)
-        bottomNavigationView.selectedItemId = R.id.connectFragment
+
+        bottomNavigationView.menu.findItem(R.id.connectFragment).isChecked = true
 
         with(LocalBroadcastManager.getInstance(this)) {
             registerReceiver(
@@ -117,7 +118,6 @@ class OrbotActivity : BaseActivity() {
                 //we didn't find indication of root
             }
         }
-
     }
 
     override fun onBackPressed() {
