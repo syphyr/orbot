f1a9dc169a2a11e34ed453586b4bc0d063c2015d
syphyr <syphyr@gmail.com>
2025-01-24 22:47:13 +0100

    Use syphyr OrbotIPtProxy submodule

--- OrbotLib/get-orbotlib.sh
+++ OrbotLib/get-orbotlib.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -d OrbotIPtProxy ]; then
-   git clone https://github.com/guardianproject/OrbotIPtProxy
+   git clone https://github.com/syphyr/OrbotIPtProxy
 fi
 cd OrbotIPtProxy
 git fetch
