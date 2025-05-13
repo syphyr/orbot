f20a8c09a4788e857420636024497afb59bf6c85
a <dsnake@protonmail.com>
2025-05-12 20:03:54 -0400

    remove relay code from preferences.xml and OrbotService's torrc logic

--- orbotservice/src/main/java/org/torproject/android/service/OrbotService.java
+++ orbotservice/src/main/java/org/torproject/android/service/OrbotService.java
@@ -66,7 +66,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Objects;
 import java.util.StringTokenizer;
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
@@ -1173,7 +1172,6 @@ public class OrbotService extends VpnService {
     private StringBuffer processSettingsImpl(StringBuffer extraLines) throws IOException {
         logNotice(getString(R.string.updating_settings_in_tor_service));
         var prefs = Prefs.getSharedPrefs(getApplicationContext());
-        var becomeRelay = prefs.getBoolean(OrbotConstants.PREF_OR, false);
         var ReachableAddresses = prefs.getBoolean(OrbotConstants.PREF_REACHABLE_ADDRESSES, false);
         var enableStrictNodes = prefs.getBoolean("pref_strict_nodes", false);
         var entranceNodes = prefs.getString("pref_entrance_nodes", "");
@@ -1224,23 +1222,6 @@ public class OrbotService extends VpnService {
 
         } catch (Exception e) {
             showToolbarNotification(getString(R.string.your_reachableaddresses_settings_caused_an_exception_), ERROR_NOTIFY_ID, R.drawable.ic_stat_notifyerr);
-            return null;
-        }
-
-        try {
-            if (becomeRelay && (!Prefs.bridgesEnabled()) && (!ReachableAddresses)) {
-                var ORPort = Integer.parseInt(Objects.requireNonNull(prefs.getString(PREF_OR_PORT, "9001")));
-                var nickname = prefs.getString(PREF_OR_NICKNAME, "Orbot");
-                var dnsFile = writeDNSFile();
-
-                extraLines.append("ServerDNSResolvConfFile").append(' ').append(dnsFile).append('\n'); // DNSResolv is not a typo
-                extraLines.append("ORPort").append(' ').append(ORPort).append('\n');
-                extraLines.append("Nickname").append(' ').append(nickname).append('\n');
-                extraLines.append("ExitPolicy").append(' ').append("reject *:*").append('\n');
-
-            }
-        } catch (Exception e) {
-            showToolbarNotification(getString(R.string.your_relay_settings_caused_an_exception_), ERROR_NOTIFY_ID, R.drawable.ic_stat_notifyerr);
             return null;
         }
 
