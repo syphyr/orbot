1373e95a74f41bf991cd739b82ae586a3ba2c7e7
syphyr <syphyr@gmail.com>
2025-05-13 23:27:53 +0200

    Add support for relay module
    This reverts commit 72a5b1326312c27f00a8688a249b942c566c3a82.

--- orbotservice/src/main/java/org/torproject/android/service/OrbotConstants.kt
+++ orbotservice/src/main/java/org/torproject/android/service/OrbotConstants.kt
@@ -5,6 +5,9 @@ import org.torproject.jni.TorService
 object OrbotConstants {
     const val TAG = "Orbot"
 
+    const val PREF_OR = "pref_or"
+    const val PREF_OR_PORT = "pref_or_port"
+    const val PREF_OR_NICKNAME = "pref_or_nickname"
     const val PREF_REACHABLE_ADDRESSES = "pref_reachable_addresses"
     const val PREF_REACHABLE_ADDRESSES_PORTS = "pref_reachable_addresses_ports"
 
