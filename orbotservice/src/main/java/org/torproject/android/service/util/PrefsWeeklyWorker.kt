120b9a2c1baa3e0b26de07481fbfc2e92f9bd5d4
a <dsnake@protonmail.com>
2025-05-14 18:16:43 -0400

    trivial to convert PrefsWeeklyWorker.java -> kotlin

---
+++ orbotservice/src/main/java/org/torproject/android/service/util/PrefsWeeklyWorker.kt
@@ -0,0 +1,13 @@
+package org.torproject.android.service.util
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+class PrefsWeeklyWorker(context: Context, workerParams: WorkerParameters) :
+    Worker(context, workerParams) {
+    override fun doWork(): Result {
+        Prefs.resetSnowflakesServedWeekly()
+        return Result.success()
+    }
+}
