3ce46d7d683eb4248bca1b05dbb5873ab4484f14
a <dsnake@protonmail.com>
2025-05-14 18:21:28 -0400

    trivial to convert DNSResolver.java -> kotlin

---
+++ orbotservice/src/main/java/org/torproject/android/service/vpn/DNSResolver.kt
@@ -0,0 +1,28 @@
+package org.torproject.android.service.vpn
+
+import java.io.IOException
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+
+class DNSResolver(private val mPort: Int) {
+    private var mLocalhost: InetAddress? = null
+
+    @Throws(IOException::class)
+    fun processDNS(payload: ByteArray): ByteArray {
+        if (mLocalhost == null) mLocalhost = InetAddress.getLocalHost()
+
+        var packet = DatagramPacket(payload, payload.size, mLocalhost, mPort)
+        val datagramSocket = DatagramSocket()
+        datagramSocket.send(packet)
+
+        // Await response from DNS server
+        val buf = ByteArray(1024)
+        packet = DatagramPacket(buf, buf.size)
+        datagramSocket.receive(packet)
+
+        datagramSocket.close()
+
+        return packet.data
+    }
+}
