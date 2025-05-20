bf404a6bd81cfd67f6da7fa254ed2d6dc55f18f8
a <dsnake@protonmail.com>
2025-05-19 22:59:12 -0400

    more item text is off center when text size is huge #1172

--- app/src/main/java/org/torproject/android/MoreFragment.kt
+++ app/src/main/java/org/torproject/android/MoreFragment.kt
@@ -2,7 +2,6 @@ package org.torproject.android
 
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
