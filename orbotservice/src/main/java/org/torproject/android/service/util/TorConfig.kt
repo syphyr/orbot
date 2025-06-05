1e6f9a70bd8e51c6d256937e7814b7efcda08335
syphyr <syphyr@gmail.com>
2025-06-05 02:37:11 +0200

    Check for reduced padding before starting relay
    Tor relays conflicts with reduced connection and circuit padding,
    so check if its enabled before starting the relay.

--- orbotservice/src/main/java/org/torproject/android/service/util/TorConfig.kt
+++ orbotservice/src/main/java/org/torproject/android/service/util/TorConfig.kt
@@ -63,14 +63,16 @@ object TorConfig {
             conf.add("ConnectionPadding 1")
         }
 
-        if (prefs?.getBoolean(PREF_REDUCED_CONNECTION_PADDING, true) ?: true) {
+        val reducedConnectionPadding = prefs?.getBoolean(PREF_REDUCED_CONNECTION_PADDING, true) ?: true
+        if (reducedConnectionPadding) {
             conf.add("ReducedConnectionPadding 1")
         }
 
         val circuitPadding = prefs?.getBoolean(PREF_CIRCUIT_PADDING, true) ?: true
         conf.add("CircuitPadding ${if (circuitPadding) "1" else "0"}")
 
-        if (prefs?.getBoolean(PREF_REDUCED_CIRCUIT_PADDING, true) ?: true) {
+        val reducedCircuitPadding = prefs?.getBoolean(PREF_REDUCED_CIRCUIT_PADDING, true) ?: true
+        if (reducedCircuitPadding) {
             conf.add("ReducedCircuitPadding 1")
         }
 
@@ -152,7 +154,8 @@ object TorConfig {
         }
 
         val becomeRelay = prefs?.getBoolean(PREF_OR, false) ?: false
-        if (becomeRelay && transport == Transport.NONE && !reachableAddresses) {
+        if (becomeRelay && transport == Transport.NONE && !reachableAddresses && !reducedConnectionPadding &&
+                !reducedCircuitPadding) {
             val orport = prefs?.getString(PREF_OR_PORT, null) ?: "9001"
             val nickname = prefs?.getString(PREF_OR_NICKNAME, "OrbotRelay")
             conf.add("ORPort $orport")
