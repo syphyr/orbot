3305f58e5b75e1e46128d5d4ed7bfe2e1e33897e
a <dsnake@protonmail.com>
2025-06-17 20:50:10 -0400

    Fix regression from #1305 The new kotlin converter is not neutral, previously nullable types now need to be explicitly set to a null default in the constructor or a kotlinx.serialization.MissingFieldException will be thrown

--- app/src/main/java/org/torproject/android/circumvention/SettingsResponse.kt
+++ app/src/main/java/org/torproject/android/circumvention/SettingsResponse.kt
@@ -2,7 +2,7 @@ package org.torproject.android.circumvention
 
 import kotlinx.serialization.Serializable
 
-@Serializable data class SettingsResponse(val settings: List<Bridges>?, val errors: List<Error>?)
+@Serializable data class SettingsResponse(val settings: List<Bridges>?, val errors: List<Error>? = null)
 @Serializable data class Bridges(val bridges: Bridge)
 @Serializable data class Bridge(val type: String, val source: String, val bridge_strings: List<String>? = null)
 @Serializable data class Error(val code: Int, val detail: String)
