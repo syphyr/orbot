4b40c03ec2acff062b3c681f23b0d3bd5617bb6d
a <dsnake@protonmail.com>
2025-06-29 14:59:31 -0400

    remove intentintegrator top level module, only the app-tv package uses it so place code in there so it still builds

--- app-tv/src/main/java/org/torproject/android/tv/IntentResult.java
+++ app-tv/src/main/java/org/torproject/android/tv/IntentResult.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.google.zxing.integration.android;
+package org.torproject.android.tv;
 
 /**
  * <p>Encapsulates the result of a barcode scan invoked through {@link IntentIntegrator}.</p>
