8fc7d69f9307be6370b45bca27f3cf15e174aa83
syphyr <syphyr@gmail.com>
2025-08-27 23:03:37 +0200

    Add contact info for non-exit relay

--- app/src/main/java/org/torproject/android/util/Prefs.kt
+++ app/src/main/java/org/torproject/android/util/Prefs.kt
@@ -345,6 +345,9 @@ object Prefs {
     val nickname: String?
         get() = cr?.getPrefString(OrbotConstants.PREF_OR_NICKNAME)
 
+    val email: String?
+        get() = cr?.getPrefString(OrbotConstants.PREF_OR_EMAIL)
+
     val customTorRc: String?
         get() = cr?.getPrefString("pref_custom_torrc")
 
