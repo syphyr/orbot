df2b779d46bdf28f81f08decafd866ebc929f35d
syphyr <syphyr@gmail.com>
2025-09-30 00:46:02 +0200

    Support building IPtProxy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -136,6 +136,7 @@ android.applicationVariants.all {
 }
 
 dependencies {
+    implementation(project(":OrbotLib"))
     implementation(libs.android.material)
     implementation(libs.android.volley)
     implementation(libs.androidx.activity)
@@ -156,7 +157,6 @@ dependencies {
     implementation(libs.appiconnamechanger)
     implementation(libs.androidx.work.kotlin)
     implementation(libs.upnp)
-    implementation(libs.iptproxy)
     implementation(files("../libs/geoip.jar"))
     api(libs.guardian.jtorctl)
     api(libs.tor.android)
