ecf1e4e290198271940b79f3a5ac3292067e3365
syphyr <syphyr@gmail.com>
2025-10-06 20:47:26 +0200

    Update versionCode for tor 0.4.8.19

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 kotlin { jvmToolchain(21) }
 
-val orbotBaseVersionCode = 1760301101
+val orbotBaseVersionCode = 1760301102
 fun getVersionName(): String {
     // Gets the version name from the latest Git tag
     return providers.exec {
