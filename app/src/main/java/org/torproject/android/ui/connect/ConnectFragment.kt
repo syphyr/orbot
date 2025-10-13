fb76f9c50ce576c870cea9cf73fd212bd7f3816a
syphyr <syphyr@gmail.com>
2025-10-13 21:01:06 +0200

    Reduce Orbit animation cycles

--- app/src/main/java/org/torproject/android/ui/connect/ConnectFragment.kt
+++ app/src/main/java/org/torproject/android/ui/connect/ConnectFragment.kt
@@ -340,13 +340,13 @@ class ConnectFragment : Fragment(),
 
         binding.ivStatus.setImageResource(R.drawable.torstarting)
         val animHover = AnimationUtils.loadAnimation(context, R.anim.hover)
-        animHover.repeatCount = 7
+        animHover.repeatCount = 3
         animHover.repeatMode = Animation.REVERSE
         binding.ivStatus.animation = animHover
         animHover.start()
 
         val animShadow = AnimationUtils.loadAnimation(context, R.anim.shadow)
-        animShadow.repeatCount = 7
+        animShadow.repeatCount = 3
         animShadow.repeatMode = Animation.REVERSE
         binding.ivShadow.animation = animShadow
         animShadow.start()
