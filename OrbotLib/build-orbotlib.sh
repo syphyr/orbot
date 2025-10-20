66b3134f7387f2a4dfad9f77a0dda6bfd6581407
syphyr <syphyr@gmail.com>
2025-10-20 22:04:23 +0200

    Update OrbotLib to java 25

--- OrbotLib/build-orbotlib.sh
+++ OrbotLib/build-orbotlib.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-sudo update-java-alternatives -s java-1.17.0-openjdk-amd64
+export ANDROID_NDK_HOME="/usr/local/src/androidSDK/android-sdk-linux/ndk/29.0.14206865"
+export ANDROID_NDK_ROOT="$ANDROID_NDK_HOME"
+
+echo "NDK is set to $ANDROID_NDK_ROOT"
+
+sudo update-java-alternatives -s java-1.25.0-openjdk-amd64
 java -version
 javac -version
 
