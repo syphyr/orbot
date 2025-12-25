3dd3e859e6475c4a035f34d8d6a6125554d3ab15
a <dsnake@protonmail.com>
2025-12-25 02:29:51 -0500

    removed unused impots on connect fragment

--- app/src/main/java/org/torproject/android/ui/connect/ConnectFragment.kt
+++ app/src/main/java/org/torproject/android/ui/connect/ConnectFragment.kt
@@ -4,13 +4,9 @@ import android.app.AlarmManager
 import android.content.Context
 import android.content.Intent
 import android.content.res.ColorStateList
-import android.graphics.Paint
 import android.net.VpnService
 import android.os.Build
 import android.os.Bundle
-import android.text.SpannableStringBuilder
-import android.text.Spanned
-import android.text.style.AbsoluteSizeSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
