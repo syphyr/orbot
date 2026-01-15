c85edc3a500e5a4b8667ed7a2eb80c25d3ce873d
a <dsnake@protonmail.com>
2026-01-15 17:37:07 -0500

    remove toplevel jitpack repository from settings.gradle, as we no longer need it after rewrite in #1560

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
         maven { url = uri("https://raw.githubusercontent.com/guardianproject/gpmaven/master") }
     }
 }
