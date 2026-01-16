1e0a8095604b5b9cce5e35faf6f77d66a269c36d
n8fr8 <nathan@freitas.net>
2026-01-16 09:24:14 -0500

    update to 1780300100

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 kotlin { jvmToolchain(21) }
 
-val orbotBaseVersionCode = 1780200100
+val orbotBaseVersionCode = 1780300100
 fun getVersionName(): String {
     // Gets the version name from the latest Git tag
     return providers.exec {
