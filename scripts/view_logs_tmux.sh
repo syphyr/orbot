4cc15f1916a0dffc011b5bcac7e1f22011724e47
a <dsnake@protonmail.com>
2026-01-20 20:35:15 -0500

    Added shell script to tmux logs from both app + tor processes via adb logcat

---
+++ scripts/view_logs_tmux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# sudo apt install tmux
+# brew install tmux
+#etc
+
+tmux new-session -s 'Orbot' -d 'adb logcat  --pid=$(adb shell pidof -s "org.torproject.android.debug") -v color'\; split-window -v 'adb logcat  --pid=$(adb shell pidof -s "org.torproject.android.debug:tor") -v color'\; attach
