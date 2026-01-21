969a9ed969d4b428cc281d58c636d52010295e17
a <dsnake@protonmail.com>
2026-01-20 21:11:36 -0500

    for #1565 should only include 32 + 64 bit arm ABIs, no intel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,10 @@ android {
         abi {
             isEnable = true
             reset()
+
+            // https://github.com/guardianproject/orbot-android/issues/1565
+            // include("armeabi-v7a", "arm64-v8a")
+
             include("x86", "armeabi-v7a", "x86_64", "arm64-v8a")
             isUniversalApk = true
         }
