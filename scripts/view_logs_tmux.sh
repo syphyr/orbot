0d6c5b7d17829179f5fd236821b4542514e316e3
a <dsnake@protonmail.com>
2026-01-22 12:32:08 -0500

    documented new logging changes in README per tla's suggestion

--- scripts/view_logs_tmux.sh
+++ scripts/view_logs_tmux.sh
@@ -3,4 +3,4 @@
 # brew install tmux
 #etc
 
-tmux new-session -s 'Orbot' -d 'adb logcat  --pid=$(adb shell pidof -s "org.torproject.android.debug") -v color'\; split-window -v 'adb logcat  --pid=$(adb shell pidof -s "org.torproject.android.debug:tor") -v color'\; set -g mouse on\; attach
+tmux new-session -s 'App&Tor' -d 'adb logcat  --pid=$(adb shell pidof -s "org.torproject.android.debug") -v color'\; split-window -v 'adb logcat  --pid=$(adb shell pidof -s "org.torproject.android.debug:tor") -v color'\; set -g mouse on\; attach
