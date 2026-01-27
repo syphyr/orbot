336c5500e6efa431ccbd2f610e47d5f2c3f0abd3
syphyr <syphyr@gmail.com>
2026-01-27 22:12:18 +0100

    Show detected dnstt type as a chip

--- app/src/main/java/org/torproject/android/ui/connect/CustomBridgeBottomSheet.kt
+++ app/src/main/java/org/torproject/android/ui/connect/CustomBridgeBottomSheet.kt
@@ -195,6 +195,7 @@ class CustomBridgeBottomSheet : OrbotBottomSheetDialogFragment() {
             "meek_lite" -> "meek_lite"
             "snowflake" -> "snowflake"
             "webtunnel" -> "webtunnel"
+            "dnstt" -> "dnstt"
             else -> {
                 if (isValidBridge(firstLine)) "vanilla" else null
             }
