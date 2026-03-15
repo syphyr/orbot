88c29c23e3073ea79526c466edd47825744bc74f
syphyr <syphyr@gmail.com>
2026-03-15 01:47:38 +0100

    Remove deprecation warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,3 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.serialization) apply false
 }
-
-allprojects {
-    tasks.withType<JavaCompile> {
-        options.compilerArgs.addAll(arrayOf("-parameters", "-Xlint:deprecation", "-Xlint:unchecked"))
-    }
-}
