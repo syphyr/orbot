cc01a65158b7d83955c14a1e14a08d089a3e4025
bitmold <dsnake@protonmail.com>
2026-03-24 03:35:47 -0400

    Remove deprecated code from tile service, refactor package

---
+++ app/src/main/java/org/torproject/android/ui/quicksettingtiles/LaunchOrbotTileService.kt
@@ -0,0 +1,21 @@
+package org.torproject.android.ui.quicksettingtiles
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.service.quicksettings.TileService
+import androidx.core.service.quicksettings.PendingIntentActivityWrapper
+import androidx.core.service.quicksettings.TileServiceCompat
+import org.torproject.android.OrbotActivity
+
+class LaunchOrbotTileService : TileService() {
+    override fun onClick() {
+        super.onClick()
+        val intent = Intent(this, OrbotActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        }
+
+        val pendingIntent =
+            PendingIntentActivityWrapper(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT, false)
+        TileServiceCompat.startActivityAndCollapse(this, pendingIntent);
+    }
+}
