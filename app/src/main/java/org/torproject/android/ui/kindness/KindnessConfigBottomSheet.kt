97f07cdd6687b99f023288cfb81404389082f0ed
Benjamin Erhart <berhart@netzarchitekten.com>
2026-04-27 14:46:12 +0200

    Issue #1509: Reworked Usage Limits bottom sheet.

--- app/src/main/java/org/torproject/android/ui/kindness/KindnessConfigBottomSheet.kt
+++ app/src/main/java/org/torproject/android/ui/kindness/KindnessConfigBottomSheet.kt
@@ -4,39 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.setFragmentResult
-import org.torproject.android.R
-import org.torproject.android.util.Prefs
+import org.torproject.android.databinding.KindnessConfigBottomSheetBinding
 import org.torproject.android.ui.OrbotBottomSheetDialogFragment
+import org.torproject.android.util.Prefs
 
 class KindnessConfigBottomSheet : OrbotBottomSheetDialogFragment() {
 
-    private lateinit var btnAction: Button
+    private lateinit var mBinding: KindnessConfigBottomSheetBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        val v = inflater.inflate(R.layout.kindess_config_bottom_sheet, container, false)
-        v.findViewById<View>(R.id.tvCancel).setOnClickListener { dismiss() }
-        btnAction = v.findViewById(R.id.btnAction)
+    ): View {
+        mBinding = KindnessConfigBottomSheetBinding.inflate(inflater, container, false)
 
-        val configWifi = v.findViewById<SwitchCompat>(R.id.swKindnessConfigWifi)
-        val configCharging = v.findViewById<SwitchCompat>(R.id.swKindnessConfigCharging)
+        mBinding.tvCancel.setOnClickListener { dismiss() }
 
-        btnAction.setOnClickListener {
-            Prefs.setBeSnowflakeProxyLimitWifi(configWifi.isChecked)
-            Prefs.setBeSnowflakeProxyLimitCharging(configCharging.isChecked)
+        mBinding.btnAction.setOnClickListener {
+            Prefs.setBeSnowflakeProxyLimitWifi(mBinding.swKindnessConfigWifi.isChecked)
+            Prefs.setBeSnowflakeProxyLimitCharging(mBinding.swKindnessConfigCharging.isChecked)
+
             setFragmentResult(BUNDLE_KEY_CONFIG_CHANGED, Bundle())
             dismiss()
         }
 
-        configWifi.isChecked = Prefs.limitSnowflakeProxyingWifi()
-        configCharging.isChecked = Prefs.limitSnowflakeProxyingCharging()
-        return v
+        mBinding.swKindnessConfigWifi.isChecked = Prefs.limitSnowflakeProxyingWifi()
+        mBinding.swKindnessConfigCharging.isChecked = Prefs.limitSnowflakeProxyingCharging()
+
+        return mBinding.root
     }
+
+    override fun getHeightRatio(): Float = 1/2f
 
     companion object {
         const val BUNDLE_KEY_CONFIG_CHANGED = "kindness_config_changed"
