6a7e82b191a48b7aa47e8f033fca8466a3c8774c
syphyr <syphyr@gmail.com>
2026-08-05 23:41:58 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -1,18 +1,17 @@
-sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
+sudo update-java-alternatives -s java-1.11.0-openjdk-amd64
 java -version
 javac -version
 
-unset ANDROID_NDK_HOME
+export ANDROID_NDK_HOME=$ANDROID_HOME/ndk/29.0.14206865
+export ANDROID_NDK_ROOT=$ANDROID_NDK_HOME
+export PATH=$ANDROID_NDK_HOME:$PATH
 
 export PKG_CONFIG_LIBDIR="$(pwd)/external/lib/pkgconfig"
-
-export ANDROID_TOOLCHAIN_HOME="$HOME/src/android-ndk-r21e"
-
-export PATH=$ANDROID_TOOLCHAIN_HOME:$PATH
 
 cd orbotservice/src/main
 ndk-build NDK_PROJECT_PATH=.
 cd ../../..
+make -C external -f build-tools
 make -C external
 #android update project --name Orbot --target android-23 --path .
 #./gradlew assemble
