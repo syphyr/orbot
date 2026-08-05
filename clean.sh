e6f481ea1e4730e7f9a973c8f5d109b0b41095ed
syphyr <syphyr@gmail.com>
2026-08-05 23:37:31 +0200

    Fix clean script

--- clean.sh
+++ clean.sh
@@ -1,15 +1,5 @@
-sudo update-java-alternatives -s java-1.8.0-openjdk-amd64
-java -version
-javac -version
-
-export ANDROID_TOOLCHAIN_HOME=~/src/android-ndk-r21d
-export ANDROID_NDK_HOME=~/toolchains/arm-linux-androideabi-4.9
-export PATH=$ANDROID_NDK_HOME/bin:$ANDROID_TOOLCHAIN_HOME:$PATH
-
-cd orbotservice/src/main
-ndk-build NDK_PROJECT_PATH=. clean
-cd ../../..
-make -C external clean
-./gradlew clean
-./gradlew --stop
-rm -rf build
+git clean -fdx
+git submodule foreach --recursive git clean -xfd
+git reset --hard
+git submodule foreach --recursive git reset --hard
+git describe --tags --always
